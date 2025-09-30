e39d17b5da020188edd2f021277615f2a4a14f1b
Hyunji <hyunhyun0096@naver.com>
2025-09-30 17:31:32 +0900

    20250930 nextjs

---
+++ section2/src/components/global-layout.tsx
@@ -0,0 +1,21 @@
+import {ReactNode} from "react";
+import Link from "next/link";
+import style from "./global-layout.module.css";
+
+export default function GlobalLayout({
+  children,
+}: {
+children:ReactNode;}){
+    return(
+<div className={style.container}>
+   <header className={style.header}>
+    <Link href={'/'}>onebite books</Link>
+   </header>
+    <main className={style.main}>
+      {children}
+    </main>
+    <footer className={style.footer}>제작 @catsmanager </footer>
+ 
+</div>
+    );
+}
---
+++ section2/src/pages/search/index.tsx
@@ -0,0 +1,22 @@
+import {useRouter} from"next/router";
+import {ReactNode} from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
+
+import SearchableLayout from "@/components/searchable-layout";
+
+
+
+export default function Page(){
+    return(
+        <div>
+            {books.map((book)=>(
+                <BookItem key={book.id}{...book}/>
+            ))}
+        </div>
+    );
+}
+
+Page.getLayout = (page:ReactNode)=>{
+    return <SearchableLayout>{page}</SearchableLayout>;
+};
