b779567e817c71958d5e60b5d1ac726f08508246
Hyunji <hyunhyun0096@naver.com>
2025-11-17 23:22:28 +0900

    section 05 initial setting

---
+++ section05/src/app/layout.tsx
@@ -0,0 +1,39 @@
+import "./globals.css";
+import Link from "next/link";
+import style from "./layout.module.css";
+import {BookData} from "@/types";
+
+async function Footer(){
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+  if(!response.ok)
+  {return <footer></footer>}
+
+  const books: BookData[] = await response.json();
+
+  const bookCount = books.length;
+  return (<footer>
+    <div>제작</div>
+    <div>{bookCount}개의 도서가 등록 되었어요</div>
+  </footer>);
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <div className={style.container}>
+          <header>
+            <Link href={"/"}>📚 ONEBITE BOOKS</Link>
+          </header>
+          <main>{children}</main>
+          <Footer/>
+        </div>
+      </body>
+    </html>
+  );
+}
